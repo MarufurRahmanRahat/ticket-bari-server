3e48e1ef5ca0076f4864466aa913107741a038a4
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-12-11 12:04:19 +0600

    updated the User.js file

--- src/models/User.js
+++ src/models/User.js
@@ -49,3 +49,36 @@ const userSchema = new mongoose.Schema(
     timestamps: true, // Adds createdAt and updatedAt
   }
 );
+
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  // Only hash if password is modified
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  // Hash password with bcrypt
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+
+// Method to get user data without sensitive info
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
